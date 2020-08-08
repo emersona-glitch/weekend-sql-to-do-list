9f00203551b87986328034f63bdeb0b111643462
Emerson Aagaard <emerson.aagaard@gmail.com>
2020-08-08 17:52:34 -0500

    pool module

---
+++ server/routes/todo.router.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
+// use pool as a module stored in this directory
+const pool = require('../modules/pool');
+
+// const config = {
+//     database: 'weekened-to-do-app',
+//     host: 'localhost',
+//     port: 5432,
+//     max: 10,
+//     idleTimeoutMillis: 10000
+// }
+
+// // const pool = new pg.Pool(config);
+
+// pool.on('connect', () => {
+//     console.log('connected to postgres');
+// });
+
+// pool.on('error', (error) => {
+//     console.log('error connecting to postgres: ' + error);
+// });
+
+
+
+// CREATE -- (post) -- (insert into)
+let queryText = `
+    INSERT INTO "koalas"
+        ("name", "name", "description", "entered")
+    VALUES
+        ($1, $2, $3, transaction_timestamp);
+`;
+
+// READ -- (get) -- (select from)
+let queryText = `
+    SELECT * FROM "tasks" ORDER BY "entered";
+`;
+
+// UPDATE -- (put) -- (update set)
+let queryText = `
+    UPDATE "koalas"
+    SET "completed" = transaction_timestamp);
+    WHERE id = $1;
+`;
+
+// DELETE -- (delete) -- (delete from)
+// delete single entry
+let queryText = `
+    DELETE FROM "koalas"
+    WHERE "id" = $1;
+`;
+
+// delete completed entries
+let queryText = `
+    DELETE FROM "koalas"
+    WHERE "completed"
+`
