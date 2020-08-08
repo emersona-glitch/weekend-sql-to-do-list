cd15ae83bd0c32077968b46d4465f38f8ec2952d
Emerson Aagaard <emerson.aagaard@gmail.com>
2020-08-08 17:16:31 -0500

    server.js setup

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,10 @@
-console.log('js');
-
 $(document).ready( onReady );
-
+// Perform all of these functions on document load
 function onReady () {
     clickListeners();
 }
 
+// All click listeners collected in one place!
 function clickListeners () {
     $('#submitButton').on('click', submitTask)
 }
@@ -17,6 +16,8 @@ function submitTask () {
         <td>${$('#descriptionInput').val()}</td>
         <td>hi</td>
         <td>no</td>
+        <td><button id="deleteButton">delete</button></td>
+        <td><button id="completeButton">complete</button></td>
         </tr>
     `);
     $('#taskInput').val('')
---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 5000;
+const router = require('./routes/todo.router');
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static('server/public'));
+
+// ROUTES
+app.use('/tasks', router);
+
+// LISTENING
+app.listen(PORT, () => {
+    console.log('listening on port: ', PORT);
+});
